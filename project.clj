d8e0028bbc1c6ac2163e1f82c4ab2067227df2c1
Radon Rosborough <radon.neon@gmail.com>
2016-07-09 17:58:09 -0600

    Add puzzles.core
    Add a :main namespace so that all the namespaces can be accessed easily
    as soon as a REPL is started.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject puzzles "0.1.0-SNAPSHOT"
   :description "Solvers for various puzzles"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main puzzles.core)
---
+++ src/puzzles/core.clj
@@ -0,0 +1,7 @@
+(ns puzzles.core
+  "Main namespace; includes all other namespaces."
+  (:require [puzzles.kenken :as kenken]
+            [puzzles.sample.kenken :as sample-kenken]
+            [puzzles.sample.sudoku :as sample-sudoku]
+            [puzzles.sudoku :as sudoku]
+            [puzzles.util :as util]))
