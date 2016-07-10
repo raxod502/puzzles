c334269f306cef1bceaa0f6c33dadb3c57fde36a
Radon Rosborough <radon.neon@gmail.com>
2016-07-10 14:37:55 -0600

    Remove the :main declaration from project.clj
    It doesn't make sense for libraries to have a :main namespace set, and removing
    this declaration eliminates the potentially confusing warning message "Warning:
    The Main-Class specified does not exist within the jar. It may not be
    executable as expected. A gen-class directive may be missing in the namespace
    which contains the main method" generated by 'lein uberjar' when deploying to
    Heroku.

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
                  [compojure "1.5.1"]
                  [environ "1.0.3"]
                  [ring/ring-jetty-adapter "1.5.0"]]
-  :main puzzles.core
   :min-lein-version "2.0.0"
   :uberjar-name "puzzles-standalone.jar"
   :profiles {:uberjar {:aot :all}})
