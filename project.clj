6114efe62784dcc8e72feb60f5f6fb8ead6d7947
Radon Rosborough <radon.neon@gmail.com>
2016-07-09 18:13:48 -0600

    Improve project description in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject puzzles "0.1.0-SNAPSHOT"
-  :description "Solvers for various puzzles"
+  :description "Solvers for KenKen and Sudoku puzzles."
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main puzzles.core)
