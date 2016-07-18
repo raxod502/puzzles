70d3e1cdaf694d28b691397b33c36f4e6b463044
Radon Rosborough <radon.neon@gmail.com>
2016-07-18 07:06:14 -0600

    Add MIT License

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject raxod502/puzzles "0.1.0-SNAPSHOT"
   :description "Solvers for KenKen and Sudoku puzzles."
+  :url "https://github.com/raxod502/puzzles"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
 
   :dependencies [;; Language
                  [org.clojure/clojure "1.8.0"]
